1f5438e06976e811ac7e8ebbf5fe729fb42f6ebf
SaschaNaz <saschanaz@outlook.com>
2014-08-28 02:24:59 +0900

    first commit, from ScaredPoors

---
+++ eventpromise.js
@@ -0,0 +1,36 @@
+﻿var EventPromise;
+(function (EventPromise) {
+    function waitEvent(target, eventName) {
+        return new Promise(function (resolve, reject) {
+            var eventListener = function (event) {
+                target.removeEventListener(eventName, eventListener);
+                resolve(event);
+            };
+            target.addEventListener(eventName, eventListener);
+        });
+    }
+    EventPromise.waitEvent = waitEvent;
+
+    function subscribeEvent(target, eventName, listener) {
+        var oncessation;
+        var subscription = {
+            cease: function () {
+                target.removeEventListener(eventName, eventListener);
+                oncessation();
+            },
+            cessation: new Promise(function (resolve, reject) {
+                oncessation = function () {
+                    resolve(undefined);
+                };
+            })
+        };
+
+        var eventListener = function (event) {
+            listener.call(target, event, subscription);
+        };
+        target.addEventListener(eventName, eventListener);
+        return subscription;
+    }
+    EventPromise.subscribeEvent = subscribeEvent;
+})(EventPromise || (EventPromise = {}));
+//# sourceMappingURL=eventpromise.js.map
---
+++ eventpromise.ts
@@ -0,0 +1,37 @@
+﻿module EventPromise {
+    export function waitEvent<T extends Event>(target: EventTarget, eventName: string) {
+        return new Promise<T>((resolve, reject) => {
+            var eventListener = (event: T) => {
+                target.removeEventListener(eventName, eventListener);
+                resolve(event);
+            };
+            target.addEventListener(eventName, eventListener);
+        });
+    }
+
+    export function subscribeEvent<T extends Event>(target: EventTarget, eventName: string, listener: (ev: T, subscription: EventSubscription) => any) {
+        var oncessation: () => any;
+        var subscription: EventSubscription = {
+            cease() {
+                target.removeEventListener(eventName, eventListener);
+                oncessation();
+            },
+            cessation: new Promise<void>((resolve, reject) => {
+                oncessation = () => {
+                    resolve(undefined);
+                };
+            })
+        };
+
+        var eventListener = (event: T) => {
+            listener.call(target, event, subscription);
+        };
+        target.addEventListener(eventName, eventListener);
+        return subscription;
+    }
+
+    export interface EventSubscription {
+        cease(): void;
+        cessation: Promise<void>
+    }
+}
