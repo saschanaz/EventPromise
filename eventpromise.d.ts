1b675b29bf07f148e2a9167b63e74603b108a434
SaschaNaz <saschanaz@outlook.com>
2015-01-05 14:50:36 +0900

    repo minification

--- eventpromise.d.ts
+++ eventpromise.d.ts
@@ -1,4 +1,4 @@
-﻿declare module EventPromise {
+declare module EventPromise {
     function waitEvent<T extends Event>(target: EventTarget, eventName: string): Promise<T>;
     function subscribeEvent<T extends Event>(target: EventTarget, eventName: string, listener: (ev: T, subscription: EventSubscription) => any): EventSubscription;
     interface EventSubscription {
--- eventpromise.js
+++ eventpromise.js
@@ -1,4 +1,27 @@
-﻿var EventPromise;
+/*
+The MIT License(MIT)
+
+Copyright(c) 2014 SaschaNaz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+var EventPromise;
 (function (EventPromise) {
     function waitEvent(target, eventName) {
         return new Promise(function (resolve, reject) {
@@ -10,13 +33,12 @@
         });
     }
     EventPromise.waitEvent = waitEvent;
-
     function subscribeEvent(target, eventName, listener) {
         var oncessation;
         var onerror;
         var subscription = {
             cease: function (options) {
-                if (typeof options === "undefined") { options = {}; }
+                if (options === void 0) { options = {}; }
                 target.removeEventListener(eventName, eventListener);
                 if (options.error)
                     onerror(options.error);
@@ -24,15 +46,10 @@
                     oncessation();
             },
             cessation: new Promise(function (resolve, reject) {
-                oncessation = function () {
-                    return resolve();
-                };
-                onerror = function (error) {
-                    return reject(error);
-                };
+                oncessation = function () { return resolve(); };
+                onerror = function (error) { return reject(error); };
             })
         };
-
         var eventListener = function (event) {
             listener.call(target, event, subscription);
         };
